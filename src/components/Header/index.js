94d4ea7b365534d897a3045ad44a849bdca532d4
Future_mr_cool <kofmehl.lennox@gmx.de>
2023-03-24 14:12:52 +0100

    cucco 1.1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import Navigation from '../Navigation/index';
 import styles from './Header.module.css';
+import Link from "next/link";
 
 const Header = () => (
     <header>
         <div className={styles.home}>
-            <img src="cucco.png" className={styles.image}></img>
+            <Link href={"/"}/>
+
+            <a href='#'><img src="cucco.png" className={styles.image}></img></a>
             <h1 className='title'>Zelda</h1>
             <Navigation/>
         </div>
