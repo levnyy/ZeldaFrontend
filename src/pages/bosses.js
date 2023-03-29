0c54cd26db82b45cefe224c149ffc70d8049f331
Future_mr_cool <kofmehl.lennox@gmx.de>
2023-03-29 16:27:41 +0200

    cucco bosses.js added because bosses cucco is cool hihihihihih

--- src/pages/bosses.js
+++ src/pages/bosses.js
@@ -1,7 +1,10 @@
 export default function IndexPage({characters}) {
     return (
         <div>
-            <h1>Zelda</h1>
+            <h1>Bosses</h1>
+            <h2>waterblight ganon</h2><img src='ganon.png'/>
+            <h2> Hinox (oldest black Kin)</h2><img src='hinox.png'/>
+            <h2> Stone talus</h2><img src='stonetalus.png'/>
         </div>
     )
 }
