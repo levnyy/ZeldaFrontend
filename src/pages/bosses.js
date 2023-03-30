137794a1620fcd2281bf076d8084027dab449263
Future_mr_cool <kofmehl.lennox@gmx.de>
2023-03-30 10:36:43 +0200

    all ccs finished

--- src/pages/bosses.js
+++ src/pages/bosses.js
@@ -2,10 +2,35 @@ import styles from '../styles/bosses.module.css'
 export default function IndexPage({characters}) {
     return (
         <div>
-            <h1>Bosses</h1>
-            <h2>waterblight ganon</h2><img src='ganon.png' className={styles.image}/>
-            <h2> Hinox (oldest black Kin)</h2><img src='hinox.png' className={styles.image}/>
-            <h2> Stone talus</h2><img src='stonetalus.png' className={styles.image}/>
+            <h1>BOSSES</h1>
+            <div>
+                <div>
+                    <div className={styles.text}>
+                        <h2>WATERBLIGHT GANON</h2>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='ganon.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h2>HINOX (OLDEST KIN)</h2>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='hinox.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h2>STONE TALUS</h2>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='stonetalus.png' className={styles.image}/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/character.js
+++ src/pages/character.js
@@ -1,19 +1,110 @@
 import styles from '../styles/character.module.css'
-export default function IndexPage({characters}) {
+export default function IndexPage() {
     return (
         <div>
             <h1>Characters</h1>
-            <h2>baddek</h2><img src='baddek.png' className={styles.image}/>
-            <h2>bamboo</h2><img src='bamboo.png' className={styles.image}/>
-            <h2>baumar</h2><img src='baumar.png' className={styles.image}/>
-            <h2>bayge</h2><img src='bayge.png' className={styles.image}/>
-            <h2>beedle</h2><img src='beedle.png' className={styles.image}/>
-            <h2>Daruk</h2><img src='Daruk.png' className={styles.image}/>
-            <h2>Epona</h2><img src='Epona.png' className={styles.image}/>
-            <h2>rivali</h2><img src='rivali.jpg' className={styles.image}/>
-            <h2>Kairo</h2><img src='Kairo.jpg' className={styles.image}/>
-            <h2>tali</h2><img src='tali.png' className={styles.image}/>
-            <h2>prinzesszelda</h2><img src='prinzesszelda.png' className={styles.image}/>
+
+
+            <div className={styles.layout}>
+                <div>
+                    <div className={styles.text}>
+                        <h1>BADDEK</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='baddek.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>BAMBOO</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='bamboo.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>BAUMAR</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='baumar.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>BAYGE</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='bayge.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>BEEDLE</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='beedle.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>DARUK</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='Daruk.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>EPONA</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='Epona.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>RIVALI</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='rivali.jpg' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>KAIRO</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='Kairo.jpg' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>TALI</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='tali.png' className={styles.image}/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.text}>
+                        <h1>PRINZESSZELDA</h1>
+                    </div>
+                    <div className={styles.cucco}>
+                        <img src='prinzesszelda.png' className={styles.image}/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
